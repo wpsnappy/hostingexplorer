c20d15e6a04afbb6e33cce00a75f9b56447f5b30
Tharindu Pramuditha <tharindutpk@gmail.com>
2021-02-02 19:54:52 +0530

    dsdsdsds

--- header.php
+++ header.php
@@ -17,8 +17,37 @@ $container = get_theme_mod( 'understrap_container_type' );
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="theme-color" content="#5533ff" />
+	<meta name="google-site-verification" content="J9RiuJuIBAOgbn54bg9mvwEM99Y_vOdEUK968Mo37Zg" />
 	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="stylesheet" href="https://use.typekit.net/oas3kdx.css">
 	<?php wp_head(); ?>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-127472731-1"></script>
+<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+	gtag('config', 'UA-127472731-1');
+</script>
+
+<!-- Facebook Pixel Code -->
+<script>
+	!function(f,b,e,v,n,t,s)
+	{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+	n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+	if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+	n.queue=[];t=b.createElement(e);t.async=!0;
+	t.src=v;s=b.getElementsByTagName(e)[0];
+	s.parentNode.insertBefore(t,s)}(window, document,'script',
+	'https://connect.facebook.net/en_US/fbevents.js');
+	fbq('init', '682396042163207');
+	fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=682396042163207&ev=PageView&noscript=1" /></noscript>
+<!-- End Facebook Pixel Code -->
+
 </head>
 
 <body <?php body_class(); ?> <?php understrap_body_attributes(); ?>>
